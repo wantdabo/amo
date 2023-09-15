cda8428c7eaf7453920616cb8d30d9740d41b7c1
GoHo <40389925+wantdabo@users.noreply.github.com>
2023-09-16 06:21:17 +0800

    godot 项目初始化

---
+++ Scripts/Entrance.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+/// <summary>
+/// 游戏入口
+/// </summary>
+public partial class Entrance : Node
+{
+    public override void _Ready()
+    {
+        base._Ready();
+    }
+
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+    }
+}
